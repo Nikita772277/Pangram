52ecfc7d1f1863ea25ecfc3d720efbd65fa31c1f
Nikita77227 <Nikita77227@DESKTOP-029TR2H>
2022-10-22 16:39:28 +0300

    124

--- Pangram/Program.cs
+++ Pangram/Program.cs
@@ -1,50 +1,94 @@
-﻿
-char[] RuLetter = new char[] { 'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц','ъ','ь', 'ч', 'ш', 'щ' , 'а', 'я', 'у', 'ю', 'о', 'е', 'ё', 'э', 'и', 'ы' };
-char[] EnLetter = new char[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', };
-List <bool> cheks = new List <bool> ();
-int count = 0;
-bool result=true;
-void Funk()
+﻿bool result = true;
+Menu();
+void Menu()
 {
+    while (true)
+    {
+        Console.WriteLine("Выберите язык (Select a language)");
+        Console.WriteLine("1. Русский (Russian)");
+        Console.WriteLine("2. Английский (English)");
+        Console.WriteLine();
+        string choice = Console.ReadLine();
+        bool check = int.TryParse(choice, out int number);
+        if (check == true)
+        {
+            if (number == 1)
+            {
+                RuPangram();
+            }
+            else if (number == 2)
+            {
+                EnPangram();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выберите пункт из меню");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Выберите пункт из меню");
+            Console.WriteLine();
+        }
+    }
+}
+void RuPangram()
+{
+    char[] RuLetter = new char[] { 'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ъ', 'ь', 'ч', 'ш', 'щ', 'а', 'я', 'у', 'ю', 'о', 'е', 'ё', 'э', 'и', 'ы' };
     Console.WriteLine("Введите предложение");
     string enter = Console.ReadLine();
     char[] a = enter.ToCharArray();
+    foreach (char c in RuLetter)
+    {
 
-        foreach (char c2 in RuLetter)
-        {
-
-        if (!a.Contains(c2))
+        if (!a.Contains(c))
         {
             result = false;
             break;
         }
-            //foreach (char c3 in RuLetter)
-            //{
-
-
-            //    if (c2 == c3)
-            //    {
-                
-            //    count++;
-            //    }
-            //    else
-            //    {
-                    
-            //    }
-            //}
-        
-        }
-        if(result==true)
-        {
-            Console.WriteLine($"да");
-        }
+    }
+    if (result == true)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Предложение панграмма");
+        Console.WriteLine();
+    }
     else
     {
-        Console.WriteLine("нет");
+        Console.WriteLine();
+        Console.WriteLine("Предложение не панграмма");
+        Console.WriteLine();
     }
 }
-void hgu()
+void EnPangram()
 {
+    char[] EnLetter = new char[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', };
 
+    Console.WriteLine("Enter a suggestion");
+    string enter = Console.ReadLine();
+    char[] a = enter.ToCharArray();
+    foreach (char c in EnLetter)
+    {
+
+        if (!a.Contains(c))
+        {
+            result = false;
+            break;
+        }
+    }
+    if (result == true)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Pangram offer");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("The offer is not a pangram");
+        Console.WriteLine();
+    }
 }
-Funk();
